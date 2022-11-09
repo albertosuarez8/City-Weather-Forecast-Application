d3cb2157bf481879a671a285268a29074d16b1c3
Albert <alberto.suarez8@outlook.com>
2022-11-08 21:05:46 -0500

    Added API fetch.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,22 @@ var cityList = $("#city-list");
 var searchBtn = $("#search-btn");
 
 var citySearch = function () {
+    var requestURL = "https://api.openweathermap.org/data/2.5/forecast" + "?q=" + cityInput.val() + "&appid=" + config.apiKey;
+    var todayURL = "https://api.openweathermap.org/data/2.5/weather" + "?q=" + cityInput.val() + "&appid=" + config.apiKey;
+    fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    })
+    fetch(todayURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    })
     var cityBtnElement = $("<button></button>").html(cityInput.val());
     cityBtnElement.addClass("btn btn-secondary btn-padding");
     cityList.append(cityBtnElement);
